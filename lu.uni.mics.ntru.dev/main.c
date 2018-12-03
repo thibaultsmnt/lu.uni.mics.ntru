191a78316245f3a1e85d695ae183476cb8a798b4
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2018-12-03 23:27:52 +0100

    Code improvement according to meeting

--- lu.uni.mics.ntru.dev/main.c
+++ lu.uni.mics.ntru.dev/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <memory.h>
 #include "sha256.h"
 
 static int8_t X = 2;
@@ -13,14 +14,13 @@ void minRepresentation(uint8_t *c, int8_t *cP) ;
 
 int16_t pow (int16_t a, int16_t n) ;
 
-int16_t mgf1(uint8_t seed[], uint8_t min_calls_mask, uint8_t N);
+int16_t mgf1(uint8_t seed[], uint32_t min_calls_mask, uint8_t N);
+
 
 void print (uint8_t array[], uint8_t length);
 
 int main() {
     printf("Hello, World!\n");
-    const uint8_t string1[] = ">";
-    const uint8_t test[] = ">6ì@hAY›Eðš¥ãX˜Á© ÿ5×—ÀêçP";
     const uint8_t test2[] = {0xd0, 0x4c, 0x38, 0xca, 0x20, 0xa1, 0xb5, 0x5f,
                              0x3e, 0x95, 0x47, 0x1f, 0x2b, 0xb1, 0xc0, 0x6e,
                              0x70, 0xd0, 0xf0, 0x97, 0x52, 0xf4, 0x60, 0x3b,
@@ -35,12 +35,6 @@ int main() {
                              0xc6, 0xed, 0xc2, 0x3f, 0x59, 0xb2, 0x07, 0x5f,
                              0xb5, 0xb9, 0x60, 0xc7, 0x80};
 
-    uint8_t hashval[32];
-
-    uint8_t length = sizeof(test2) / sizeof(test2[0]);
-
-    //sha256_hash(hashval, test, 20);
-
     mgf1(test2, 6,20);
 
     return 0;
@@ -48,81 +42,69 @@ int main() {
 
 
 
-int16_t mgf1(uint8_t seed[], uint8_t min_calls_mask, uint8_t N){
+int16_t mgf1(uint8_t seed[], uint32_t min_calls_mask, uint8_t N){
 
     // Hash from 0 to min calls =>  min calls + 1 space needed
-    min_calls_mask = (uint8_t) (min_calls_mask + 1);
-    uint8_t Z[32];
+    uint8_t Z[36];
 
-    sha256_hash(Z, seed, 3);
+    sha256_hash(Z, seed, 65);
 
     // WARNING: may overflow
-    uint8_t bufferSize = (uint8_t) (min_calls_mask * 32);
+    uint8_t bufferSize = (uint8_t) (min_calls_mask+1 * 32);
     uint8_t *buffer = malloc(bufferSize * sizeof(uint8_t));
 
-    // Create copy of the table
-    uint8_t Zi[33];
-    for (int j = 0; j < 32; ++j) {
-        Zi[j]= Z [j];
-    }
 
     // Fill the buffer
+    uint8_t *ctr = Z +32;
+
+    // Set counter to 0
+    memset(ctr, 0, 4);
+
     for (uint8_t i = 0; i < min_calls_mask; ++i) {
-        Zi[32] = i;
-        sha256_hash((buffer + i*32), Zi, 3);
-    }
-    print(buffer, bufferSize);
-    printf("\n");
 
-    uint16_t v = 0;
-    uint8_t i = 0;
+        // Generate hash and add it to buffer
+        sha256_hash((buffer + i*32), Z, 36);
+        print(Z, 36);
 
-    for (uint8_t j = 0; j < bufferSize; ++j) {
-        uint8_t octet = buffer[j];
-        if ( octet < 243 ){
-
-            uint8_t c[5];
-            int8_t cP[5];
-            choose(octet, c);
-            print(c,5);
-            minRepresentation(c,cP);
-
-            for (uint8_t k = 0; k < 5; ++k) {
-                v = v + cP[i+k]*pow(X,i+k);
+        // Increment ctr
+        if (++ctr[3] == 0) {
+            if (++ctr[2] == 0) {
+                if (++ctr[1] == 0) {
+                    ++ctr[0];
+                }
             }
-
-            i = (uint8_t) (i + 5);
-
         }
     }
-    printf("%02x", v);
 
 
-//    uint8_t i = 0;
-//    int16_t v = 0;
-//
-//    for (int i = 0; i < min_calls_mask; ++i) {
-//        uint8_t octet = buffer[i];
-//        uint8_t *c = malloc(5* sizeof(uint8_t));
-//        int8_t *cP = malloc(5* sizeof(int8_t));
-//
-//        if (octet < 243) {
-//            choose(octet, c);
-//            minRepresentation(c,cP);
-//            int j = 0;
-//            while (j < 5) {
-//                int k = i + j;
-//                v = v + (cP[k] *  ((int16_t) (pow(X,k))));
-//                j++;
-//            }
-//            i = i + 5;
-//        }
-//    }
-//
-//    int16_t output = v %  ((int16_t) (pow(X,N)));
-//
-//
-//    free(buffer);
+
+    uint8_t *out = malloc(N* sizeof(uint8_t));
+    uint8_t *outWork = out;
+
+
+    uint8_t octet_available = bufferSize;
+    uint8_t *octet = buffer;
+
+    uint8_t workN = N;
+    while (workN){
+
+        if (octet_available == 0){
+            return 1;
+        }
+
+        if (*octet < 243){
+            choose(*octet, outWork);
+            outWork +=5;
+            workN--;
+        }
+        octet++;
+        octet_available--;
+
+    }
+
+    free(buffer);
+
+    print(out, N);
 
     return 0;
 
@@ -133,6 +115,8 @@ void print (uint8_t array[], uint8_t length){
     for (uint8_t i = 0; i < length; ++i) {
         printf("0x%02x, ", array[i]);
     }
+    printf("\n");
+
 
 }
 void choose(uint8_t octet, uint8_t *c) {
@@ -162,3 +146,5 @@ int16_t pow (int16_t a, int16_t n) {
     }
     return n;
 }
+
+
